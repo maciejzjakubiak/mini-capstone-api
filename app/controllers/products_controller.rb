c048d17cc95083f2a7d4f3a575ae18711b8c8df5
Maciej Jakubiak <maciej.z.jakubiak@gmail.com>
2022-06-30 12:38:09 -0500

    Created 4 additional path and methods for individual items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,24 @@ class ProductsController < ApplicationController
     product = Product.all
     render json: product
   end
+
+  def product1
+    product1 = Product.first
+    render json: product1
+  end
+
+  def product2
+    product2 = Product.second
+    render json: product2
+  end
+
+  def product3
+    product3 = Product.third
+    render json: product3
+  end
+
+  def product4
+    product4 = Product.fourth
+    render json: product4
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/products", controller: "products", action: "all_products"
+  get "/product1", controller: "products", action: "product1"
+  get "/product2", controller: "products", action: "product2"
+  get "/product3", controller: "products", action: "product3"
+  get "/product4", controller: "products", action: "product4"
 end
